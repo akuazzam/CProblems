5049bfe9b0196946876accd2761765737c559f58
akuazzam <72036463+akuazzam@users.noreply.github.com>
2020-10-24 01:04:17 -0500

    Create mario1.c

---
+++ mario1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main () {
+    int h = get_int ("Enter height: ");
+
+    for (int i = 0; i < h; i ++) {
+        for (int j = 0; j < h - i - 1; j ++) {
+            printf (" ");
+        }
+        for (int k = 0; k < i + 1; k ++) {
+            printf ("#");
+        }
+
+        printf ("\n");
+    }
+}
